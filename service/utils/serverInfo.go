2216fec0eedd5084168e18abbff83a6cd897a3b8
be_gdm <MengYen333@outlook.com>
2026-02-22 17:44:34 +0800

    1

--- service/utils/serverInfo.go
+++ service/utils/serverInfo.go
@@ -1,29 +1,35 @@
 package utils
 
+const baseValue = 1000
+
 /**
-* serverId: 1~ 65*1000
-* GroupId:1~65
+* serverId: GroupId
+* (0~999):1 （1000~1999):2 (2000~2999):3
  */
 func GetGroupIdByServerId(serverId uint32) uint32 {
-	return (serverId / 1000) + 1 // (1~999):1 （1000~1999):2 (2000~2999):3
+	return (serverId / baseValue) + 1
 }
 
 /**
-* pdId: 1 ~ 65535
-* GroupId:1~65
-*(1~999):1 （1000~1999):2 (2000~2999):3
+* serverId: GroupId
+* (0~999):1 （1000~1999):2 (2000~2999):3
  */
-func GetGroupIdByPb(pbId int) uint32 {
-	return uint32((pbId / 1000) + 1)
-}
-
 func GetServerIdByServerId(groupId uint32, serverIds []uint32) int {
-	maxValue := groupId*1000 - 1
-	minValue := maxValue - 1000
+	maxValue := groupId*baseValue - 1
+	minValue := maxValue - baseValue + 1
 	for _, target := range serverIds {
 		if target >= minValue && target <= maxValue {
 			return int(target)
 		}
 	}
 	return 0
+}
+
+/**
+* pdId: 1 ~ 65535
+* pdId:GroupId
+*(1~999):1 （1000~1999):2 (2000~2999):3
+ */
+func GetGroupIdByPb(pbId int) uint32 {
+	return uint32((pbId / baseValue) + 1)
 }
