bb1c4d4d1a2f01ff147e3e6abc0970635090cea1
be_gdm <MengYen333@outlook.com>
2026-02-28 17:24:28 +0800

    1

--- app/gate/main.go
+++ app/gate/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"gameServer/pkg/config"
 	"gameServer/pkg/logger/log1"
 	"gameServer/pkg/logger/log2"
 	"gameServer/service/services/gate"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
+	"github.com/spf13/pflag"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -15,9 +19,25 @@ import (
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
+
+	// os.Args[0] 程序本身的启动路径
+	//| 启动方式                   | `os.Args[0]` 的值                            |
+	//| :--------------------- | :----------------------------------------- |
+	//| `./myapp`              | `./myapp`                                  |
+	//| `/home/user/app/myapp` | `/home/user/app/myapp`                     |
+	//| `go run main.go`       | 临时编译路径（如 `/tmp/go-build123/b001/exe/main`） |
+
+	configPath := pflag.String("configPath", filepath.Join(filepath.Dir(os.Args[0]), "config"), "配置文件路径") // ./config/
+	configName := pflag.String("configName", "gate", "配置文件名称，不带后缀")                                       // gate.toml
+	logPath := pflag.String("logPath", filepath.Join(filepath.Dir(os.Args[0]), "logs"), "日志路径")           // ./logs/
+	//logLevel := pflag.String("logLevel", "debug", "日志等级 debug info warn error fatal")
+	nodeName := pflag.String("node", "gate-1", "节点名称")
+	pflag.Parse()
+
+	fmt.Println("configPath:", *configPath)
 	// 打docker的时候需要填写路径
-	log1.Init(zapcore.DebugLevel, "gate-1", "./logs", !config.Get().IsDevelop())
-	log2.Init(log2.Config{Level: zapcore.DebugLevel, LogDir: "./logs", IsDocker: false})
+	log1.Init(zapcore.DebugLevel, *nodeName, *logPath, !config.Get().IsDevelop())
+	log2.Init(log2.Config{Level: zapcore.DebugLevel, LogDir: *logPath, IsDocker: false})
 
 	//log2.Get().Debug("etcd Register==", zap.String("address=", "123123"))
 	//log2.Get().Info("etcd Register==", zap.String("address=", "44444"))
@@ -25,9 +45,12 @@ func main() {
 	//log2.Get().Error("etcd Register==", zap.String("address=", "6666"))
 
 	// [E:\gowork\gameServer]
-	if err := config.Init("gate-1", "gate", "./config", "test", "toml"); err != nil {
+	if err := config.Init(*nodeName, "gate", *configPath, *configName, "toml"); err != nil {
 		panic(err)
 	}
+	//if err := config.Init("gate-1", "gate", "./config", "gate", "toml"); err != nil {
+	//	panic(err)
+	//}
 	// pprof
 	go func() {
 		pprofAddr := config.Get().PProfAddress()
